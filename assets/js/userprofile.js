d3f145959184d6cfc41d1b444667b5003917b202
AliHamzaBham <alihamzabham@gmail.com>
2022-04-09 17:04:35 +0500

    edit profile form added

--- assets/js/userprofile.js
+++ assets/js/userprofile.js
@@ -1,15 +1,15 @@
 const editProfileForm = document.querySelector('#edit-prof-form');
-const editProfBtn  = document.querySelector('.edit-prof-btn'); 
-const editProfInput  = document.querySelectorAll('.edit-prof-input');
-const dropdownUserName = document.querySelector('.dropdown-user-name'); 
+const editProfBtn = document.querySelector('.edit-prof-btn');
+const editProfInput = document.querySelectorAll('.edit-prof-input');
+const dropdownUserName = document.querySelector('.dropdown-user-name');
 const mainUserName = document.querySelector('.main-user-name');
 const mainEmail = document.querySelector('.main-email');
 const userName = document.querySelector('.user-name');
 const country = document.querySelector('.country');
 const email = document.querySelector('.email');
-const currency = document.querySelector('.currency'); 
+const currency = document.querySelector('.currency');
 
-editProfBtn.addEventListener('click' , () => {
+editProfBtn.addEventListener('click', () => {
     editProfileForm.style.display = "block";
     editProfBtn.style.display = "none";
 })
@@ -21,14 +21,19 @@ editProfileForm.addEventListener('submit', (e) => {
     let changedcountry = editProfInput[1].value;
     let changedEmail = editProfInput[2].value;
     let changedCurrency = editProfInput[3].value;
- 
-    dropdownUserName.textContent = changedUserName;
-    mainUserName.textContent = changedUserName;
-    mainEmail.textContent = changedEmail;
-
-    userName.textContent = changedUserName;
-    country.textContent = changedcountry;
-    email.textContent = changedEmail;
+  
+    if (changedUserName.length > 0) {
+        dropdownUserName.textContent = changedUserName;
+        mainUserName.textContent = changedUserName;
+        userName.textContent = changedUserName;
+    }
+    if(changedEmail.length > 0) {
+        mainEmail.textContent = changedEmail;
+        email.textContent = changedEmail;
+    }
+    if(changedcountry.length > 0){
+        country.textContent = changedcountry;
+    } 
     currency.textContent = changedCurrency;
 
     editProfileForm.style.display = "none";
